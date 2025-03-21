eefd2010c4f2af28adaea184c05d25941d8a3d2d
BLUERAY94 <161076083+BLUERAY94@users.noreply.github.com>
2025-03-21 23:35:05 +0530

    Add files via upload
    Script Files needed to automate the Youtube-skip-ad

---
+++ add_skip.bat
@@ -0,0 +1,5 @@
+
+:: Run the script using the full path dynamically
+%~dp0.venv\Scripts\python.exe main.py
+
+pause
---
+++ cursor_position_caliberation.py
@@ -0,0 +1,23 @@
+import pyautogui, collections
+import time
+skip_ad_wait_time = 0.5  # in seconds
+
+
+CURSOR_LOC_FINDER = 1           #To know the Cursor Location, Keep this as 1. To Move the cursor to location, Keep this as 0.
+TEST              = not(CURSON_LOC_FINDER = 1)
+
+for i in range(20):
+    time.sleep(skip_ad_wait_time)
+        
+    if CURSOR_LOC_FINDER == 1:
+        # Get the current position of the cursor
+        skip_button_location    = pyautogui.position()
+        print(f"The current cursor position is: {skip_button_location}")
+        
+    if TEST == 1:
+        # Go to (x=822, y=530) and click
+        Point                   = collections.namedtuple("Point", "x y")
+        skip_button_location    = Point(x=830, y=531)
+        print(f"Skipping add at: {skip_button_location}")
+        time.sleep(skip_ad_wait_time)
+        pyautogui.click(skip_button_location)
